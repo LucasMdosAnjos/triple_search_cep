2f17058617fc8663649caa829a751e149ff23bf0
LucasMdosAnjos <lucasmon@live.com>
2022-12-25 18:04:41 -0300

    ajustes

--- test/app/modules/home/presenter/stores/search_cep_store_test.dart
+++ test/app/modules/home/presenter/stores/search_cep_store_test.dart
@@ -1,9 +1,11 @@
 import 'package:triple_search_cep/app/modules/home/domain/entity/params_search_cep.dart';
+import 'package:triple_search_cep/app/modules/home/domain/errors/errors.dart';
 import 'package:triple_search_cep/app/modules/home/domain/usecases/search_cep_usecase.dart';
 import 'package:triple_search_cep/app/modules/home/external/http/http_search_cep.dart';
 import 'package:triple_search_cep/app/modules/home/infra/repositories/search_cep_repository_impl.dart';
 import 'package:triple_search_cep/app/modules/home/presenter/stores/search_cep_store.dart';
 import 'package:triple_test/triple_test.dart';
+
 
 void main() {
   storeTest<SearchCepStore>(
