8346701ca7c165f8b8845d73debfca2b7e6ccd5a
LucasMdosAnjos <lucasmon@live.com>
2022-12-28 14:36:52 -0300

    ajustes

--- lib/app/modules/home/presenter/widgets/home.dart
+++ lib/app/modules/home/presenter/widgets/home.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_triple/flutter_triple.dart';
 import 'package:triple_search_cep/app/modules/home/presenter/blocs/search_cep_bloc/search_cep_bloc.dart';
 import 'package:triple_search_cep/app/modules/home/presenter/blocs/search_cep_bloc/search_cep_event.dart';
 
 import '../../domain/entity/params_search_cep.dart';
-import '../../domain/errors/errors.dart';
-import '../../domain/usecases/search_cep_usecase.dart';
-import '../../external/http/http_search_cep.dart';
-import '../../infra/repositories/search_cep_repository_impl.dart';
 import '../blocs/search_cep_bloc/search_cep_state.dart';
-import '../stores/search_cep_store.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
